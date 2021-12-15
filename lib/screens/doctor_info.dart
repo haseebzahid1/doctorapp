b98257793c36df46f5e8364c5d025e9630cdb64c
haseebzahid1 <mmianhaseeb444@gmail.com>
2021-12-15 12:32:10 +0500

    doctorapp

---
+++ lib/screens/doctor_info.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DoctorInfo extends StatefulWidget {
+  const DoctorInfo({Key? key}) : super(key: key);
+
+  @override
+  _DoctorInfoState createState() => _DoctorInfoState();
+}
+
+class _DoctorInfoState extends State<DoctorInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
